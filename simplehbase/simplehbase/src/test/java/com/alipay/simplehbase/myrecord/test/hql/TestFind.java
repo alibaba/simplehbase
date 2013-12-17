d8b4ca46a7ad0d0e0a27c5c733b5cba963d926aa
xinzhi.zhang <xinzhi.zhang@alipay.com>
2013-12-17 19:15:27 +0800

    add test

---
+++ simplehbase/simplehbase/src/test/java/com/alipay/simplehbase/myrecord/test/hql/TestFind.java
@@ -0,0 +1,43 @@
+package com.alipay.simplehbase.myrecord.test.hql;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.alipay.simplehbase.myrecord.MyRecord;
+import com.alipay.simplehbase.myrecord.MyRecordRowKey;
+import com.alipay.simplehbase.myrecord.test.MyRecordTestBase;
+
+public class TestFind extends MyRecordTestBase {
+
+    @Test
+    public void testFind() {
+
+        put("id=0,name=aaa");
+
+        MyRecord myRecord = simpleHbaseClient.findObject(new MyRecordRowKey(0),
+                MyRecord.class);
+
+        Assert.assertEquals(0, myRecord.getId());
+        Assert.assertEquals("aaa", myRecord.getName());
+
+        String hql = "select where name equal \"aaa\"";
+        myRecord = simpleHbaseClient.findObjectByRawHql(new MyRecordRowKey(0),
+                MyRecord.class, hql, null);
+
+        Assert.assertEquals(0, myRecord.getId());
+        Assert.assertEquals("aaa", myRecord.getName());
+
+        hql = "select where name equal #name#";
+        Map<String, Object> para = new HashMap<String, Object>();
+        para.put("name", "aaa");
+        myRecord = simpleHbaseClient.findObjectByRawHql(new MyRecordRowKey(0),
+                MyRecord.class, hql, para);
+        Assert.assertEquals(0, myRecord.getId());
+        Assert.assertEquals("aaa", myRecord.getName());
+    }
+
+}
