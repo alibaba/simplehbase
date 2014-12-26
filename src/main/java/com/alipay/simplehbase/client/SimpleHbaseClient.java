823cc54ed2b9ab71522f87512294fa05f101f611
xinzhi.zhang <xinzhi.zhang@alipay.com>
2014-12-26 22:56:56 +0800

    删除cp的相关代码。0.94和0.98不兼容。

--- src/main/java/com/alipay/simplehbase/client/SimpleHbaseClient.java
+++ src/main/java/com/alipay/simplehbase/client/SimpleHbaseClient.java
@@ -1,6 +1,5 @@
 package com.alipay.simplehbase.client;
 
-import com.alipay.simplehbase.client.service.AggregateService;
 import com.alipay.simplehbase.client.service.BasicService;
 import com.alipay.simplehbase.client.service.HBaseDataSourceAware;
 import com.alipay.simplehbase.client.service.HBaseTableConfigAware;
@@ -20,8 +19,8 @@ import com.alipay.simplehbase.client.service.SimpleHbaseVersionedService;
  * @author xinzhi
  * */
 public interface SimpleHbaseClient extends BasicService,
-        SimpleHbaseVersionedService, AggregateService, HbaseService,
-        HbaseMultipleVersionService, HBaseDataSourceAware,
-        SimpleHbaseRuntimeSettingAware, HBaseTableConfigAware, HbaseRawService {
+        SimpleHbaseVersionedService, HbaseService, HbaseMultipleVersionService,
+        HBaseDataSourceAware, SimpleHbaseRuntimeSettingAware,
+        HBaseTableConfigAware, HbaseRawService {
 
 }
